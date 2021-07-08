67847f82d7ed06220d41e0a4c3d81f273f34b1dd
Himanshu <hv65616@gmail.com>
2021-07-08 12:19:54 +0530

    Update to detect eyes and mouth correctly

--- face2.py
+++ face2.py
@@ -1,92 +1,97 @@
 # Starting of file will be importing of cv2 module which allows to do certain functions with our image
 import cv2
 
-#Then importing of OS module
-import os
+# **********************************
+# #Then importing of OS module
+# import os
+# **********************************
 
-#Here,face_classifier stores the data set of frontal face
+# Here,face_classifier stores the data set of frontal face
 face_classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-#Here,eye_classifier stores the data set of detecting of eyes
+# Here,eye_classifier stores the data set of detecting of eyes
 eye_classifier = cv2.CascadeClassifier("haarcascade_eye.xml")
 
-#Here,smile_classifier stores the data of detecting of smile on face
+# Here,smile_classifier stores the data of detecting of smile on face
 smile_classifier = cv2.CascadeClassifier("haarcascade_smile.xml")
 
-#Activating the camera and storing the video capture in cap
+# Activating the camera and storing the video capture in cap
 cap = cv2.VideoCapture(0)
 
-#Initializing of a variable with initial value of 0
-count = 0
+# *********************************************************
+# #Initializing of a variable with initial value of 0
+# count = 0
+# *********************************************************
 
 while True:
 
-    #At this line we are doing 2 things
-    #In ret we will be storing either true or false of image reading
-    #In frame we will be storing the frames that our camera produces
+    # At this line we are doing 2 things
+    # In ret we will be storing either true or false of image reading
+    # In frame we will be storing the frames that our camera produces
     ret, frame = cap.read()
 
-    #putText() used to put the text with certain compulsory parameters
-    cv2.putText(frame,"Face Detection",(200,450),cv2.FONT_HERSHEY_PLAIN,2.3,(255,255,0),2)
+    # putText() used to put the text with certain compulsory parameters
+    cv2.putText(frame, "Face Detection", (200, 450),cv2.FONT_HERSHEY_PLAIN, 2.3, (255, 255, 0), 2)
 
-    #Here, we will convert the imgae into the gray basically the pixls become either 0 or 225
+    # Here, we will convert the imgae into the gray basically the pixls become either 0 or 225
     convert_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #After converting into gray we will pass the converted image into detectMultiScale so that we can find face more appropiately
-    faces = face_classifier.detectMultiScale(convert_gray, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30))
+    # After converting into gray we will pass the converted image into detectMultiScale so that we can find face more appropiately
+    faces = face_classifier.detectMultiScale(convert_gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    #Making the suitable size frame of our face now we are assigning its height width value to variables so that we can make rectangle box
+    # Making the suitable size frame of our face now we are assigning its height width value to variables so that we can make rectangle box
     for (x, y, w, h) in faces:
 
-        #Here,we create rectangle with dimensions specified and color of box and thickness
+        # Here,we create rectangle with dimensions specified and color of box and thickness
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
 
-        #This will crop the gray_image according to dimensions passed
+        # This will crop the gray_image according to dimensions passed
         roi_gray = convert_gray[y:y+h, x:x+w]
 
-        #This will crop the original frame according to the dimensions passed
+        # This will crop the original frame according to the dimensions passed
         roi_color = frame[y:y+h, x:x+w]
 
-        #Providing the path value where we will store the captured frames
-        path = r'C:\Users\hpw\OneDrive\Documents\ImageProcessing\Output'
+# *****************The following part is for capturing frames and storing it in directory defined******************************
 
-        #Assigning the frames a name from which they will save
-        frame_number = "Frame_{}.jpg".format(count)
+        # #Providing the path value where we will store the captured frames
+        # path = r'C:\Users\hpw\OneDrive\Documents\ImageProcessing\Output'
 
-        os.chdir(path)
+        # #Assigning the frames a name from which they will save
+        # frame_number = "Frame_{}.jpg".format(count)
 
-        #This will create and save the images
-        cv2.imwrite(frame_number, roi_color)
+        # os.chdir(path)
 
-        #Updating the value of count so that name of frame get continuously updated
-        count += 1
+        # #This will create and save the images
+        # cv2.imwrite(frame_number, roi_color)
 
-        #here we are taking out only the part of our smile
-        smiles = smile_classifier.detectMultiScale(roi_gray)
+        # #Updating the value of count so that name of frame get continuously updated
+        # count += 1
+
+# ******************************************************************************************************************************
+        # here we are taking out only the part of our smile
+        smiles = smile_classifier.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=10, minSize=(15, 15))
 
         for (sx, sy, sw, sh) in smiles:
-
             cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (255, 0, 0), 2)
-        
-        #Here we are taking out part for our eye
-        eyes = eye_classifier.detectMultiScale(roi_gray)
+
+        # Here we are taking out part for our eye
+        eyes = eye_classifier.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=10, minSize=(15, 15))
 
         for (ex, ey, ew, eh) in eyes:
-
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 0, 225), 2)
 
-    #This is responsible for showing our video on screen
+    # This is responsible for showing our video on screen
     cv2.imshow("My video", frame)
 
-    #Waitkey() allows us to set the time interval in between reading and showing
-    key = cv2.waitKey(10)
+    # Waitkey() allows us to set the time interval in between reading and showing
+    key = cv2.waitKey(1)
 
-    #For exiting the window we press q key
+    # For exiting the window we press q key
     if key == ord("q"):
         break
 
-#At last release the cap
+# At last release the cap
 cap.release()
 
-#And release all the windows
+# And release all the windows
 cap.deleteAllWindows()
